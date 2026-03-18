4dac8e13be32a1792b14042740845c8024aa3c7e
Vin Harng Chew <vinharngchew@vins-imac.lan>
2026-03-17 22:03:31 -0400

    commit code

--- etl_venv/lib/python3.9/site-packages/tests/rest/serialize.py
+++
@@ -1,7 +0,0 @@
-def object_to_dict(obj):
-    if isinstance(obj, list):
-        return [object_to_dict(item) for item in obj]
-    elif hasattr(obj, "__dict__"):
-        return {key: object_to_dict(value) for key, value in obj.__dict__.items()}
-    else:
-        return obj
